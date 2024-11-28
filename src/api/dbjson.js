a89b0b486de988dcce181786de73015fcfa9bdec
cara <cara656513@gmail.com>
2024-11-28 10:56:24 +0900

    feature: 환경 변수 설정

--- src/api/dbjson.js
+++ src/api/dbjson.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const jsonResultsApi = axios.create({
-  baseURL: "http://localhost:4000/testResults",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default jsonResultsApi;
